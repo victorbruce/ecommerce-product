3bf41a6f6ccf2eb5d5186c1966012ffa21c74986
Victor Bruce <victorbruce82@gmail.com>
2024-02-05 06:02:56 +0000

    app colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "var(--orange)",
+        paleOrange: "var(--paleOrange)",
+        veryDarkBlue: "var(--veryDarkBlue)",
+        darkGrayishBlue: "var(--darkGrayishBlue)",
+        grayishBlue: "var(--grayishBlue)",
+        lightGrayishBlue: "var(--lightGrayishBlue)",
+        white: "var(--white)",
+        black: "var(--black)",
+      },
+      textColor: {
+        orange: "var(--orange)",
+        paleOrange: "var(--paleOrange)",
+        veryDarkBlue: "var(--veryDarkBlue)",
+        darkGrayishBlue: "var(--darkGrayishBlue)",
+        grayishBlue: "var(--grayishBlue)",
+        lightGrayishBlue: "var(--lightGrayishBlue)",
+        white: "var(--white)",
+        black: "var(--black)",
+      },
+    },
   },
   plugins: [],
 };
